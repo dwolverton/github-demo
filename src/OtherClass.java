20eefed648a4ed8ca68d106f0472a20895a7a8dd
David Wolverton <dwolvert@gmail.com>
2020-10-29 10:59:22 -0400

    !

--- src/OtherClass.java
+++ src/OtherClass.java
@@ -2,7 +2,7 @@
 public class OtherClass {
 
 	public static void greet(String name) {
-		System.out.println("Hello " + name + "!");
+		System.out.println("Hello " + name + "!!");
 		System.out.println("Fine day, what?");
 	}
 }
